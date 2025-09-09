34ef816e0288f97603e8db5529c2805f18fea794
Lankadinee <lankadinee@gmail.com>
2025-09-09 20:58:16 +1000

    Join mapped query implementation

--- src/colse/multi_spline_dequantizer.py
+++ src/colse/multi_spline_dequantizer.py
@@ -5,7 +5,7 @@
 from loguru import logger
 from colse.data_path import get_data_path
 from colse.dataset_names import DatasetNames
-from colse.datasets.join_dataset_info import get_table_cols
+from colse.datasets.join_dataset_info import get_all_columns, get_query_indexes, get_table_cols
 from colse.df_utils import load_dataframe
 from colse.spline_dequantizer import SplineDequantizer
 
@@ -41,6 +41,14 @@ class MultiSplineDequantizer:
         no_of_cols = len(get_table_cols(self.dataset_type)[table_name])
         return self.dequantizers[table_name].get_converted_cdf(query, column_indexes=[i for i in range(no_of_cols)], table_name=table_name)
 
-    def get_mapped_query(self, table_name, query):
-        no_of_cols = len(get_table_cols(self.dataset_type)[table_name])
-        return self.dequantizers[table_name].get_mapped_query(query, column_indexes=[i for i in range(no_of_cols)], table_name=table_name)
+    def get_mapped_query(self, full_query):
+        full_query = full_query[0]
+        all_tables = self.dataset_type.get_join_tables()
+        full_mapped_query = full_query.copy()
+        for table_name in all_tables:
+            indexes = get_query_indexes(self.dataset_type, table_name)
+            query = full_query[indexes]
+            column_indexes = [i + 1 for i in range(len(indexes)//2)]
+            mapped_query =  self.dequantizers[table_name].get_mapped_query(query.reshape(1, -1), column_indexes=column_indexes, table_name=table_name)
+            full_mapped_query[indexes] = mapped_query
+        return full_mapped_query
